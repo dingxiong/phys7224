2a1b18486ee51aeef109196a56ed188853c52403
dingxiong <dingxiong203@gmail.com>
2015-08-02 20:03:50 -0400

    big progress

--- hws/grader/models.py
+++ hws/grader/models.py
@@ -1,15 +1,67 @@
 from django.db import models
 
 
-class hw1_submit(models.Model):
+class Hw1_submit(models.Model):
     email = models.EmailField(max_length=300)
     time = models.DateTimeField()
     hasGraded = models.BooleanField(default=False)
-    q1 = models.DecimalField()
+    
+    q1 = models.CharField(max_length=500)
     q2 = models.FloatField()
     q3 = models.FloatField()
     q4 = models.FloatField()
     q5 = models.FloatField()
 
+    g1 = models.FloatField()
+    g2 = models.FloatField()
+    g3 = models.FloatField()
+    g4 = models.FloatField()
+    g5 = models.FloatField()
+
+    def __str__(self):
+        return self.email
 
 
+class Hw1_key(models.Model):
+    q1 = models.CharField(max_length=500)
+    q2 = models.FloatField()
+    q3 = models.FloatField()
+    q4 = models.FloatField()
+    q5 = models.FloatField()
+    
+
+class Hw2_submit(models.Model):
+    email = models.EmailField(max_length=300)
+    time = models.DateTimeField()
+    hasGraded = models.BooleanField(default=False)
+    
+    q1 = models.FloatField()
+    q2 = models.CharField(max_length=500)
+    q3 = models.CharField(max_length=500)
+    q4 = models.FloatField()
+    q5 = models.CharField(max_length=500)
+
+    g1 = models.FloatField(blank=True, default=0)
+    g2 = models.FloatField(blank=True, default=0)
+    g3 = models.FloatField(blank=True, default=0)
+    g4 = models.FloatField(blank=True, default=0)
+    g5 = models.FloatField(blank=True, default=0)
+    gs = models.FloatField(blank=True, default=0)    # total points
+    gp = models.FloatField(blank=True, default=0)    # percentage
+
+    def __str__(self):
+        return self.email
+
+
+class Hw2_key(models.Model):
+    qname = models.CharField(max_length=20)
+    qtitle = models.CharField(max_length=800)
+    qtype = models.CharField(max_length=20, default='number')
+    choiceAnswer = models.CharField(max_length=300, default="first")
+    numberAnswer = models.FloatField(default=0)
+    qtol = models.FloatField(default=0.001)
+    qpoints = models.FloatField()
+
+    def __str__(self):
+        return self.qname
+    
--- hws/grader/views.py
+++ hws/grader/views.py
@@ -1,5 +1,80 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.utils import timezone
+from .forms import *
+
+
+def submitted(request):
+    return render(request, 'submitted.html')
 
 
 def hw1(request):
-    return render(request, 'homework1.html')
+    if request.method == 'POST':
+        
+        return render(request, 'homework1.html',
+                      {'hwForm': Hw1Form()})
+
+    # default action
+    return render(request, 'homework1.html',
+                  {'hwForm': Hw1Form()})
+
+
+def hw2(request):
+    if request.method == 'POST':
+        hw2Form = Hw2Form(request.POST)
+        if hw2Form.is_valid():
+            email = hw2Form.cleaned_data['email']
+            q1 = hw2Form.cleaned_data['q1']
+            q2 = hw2Form.cleaned_data['q2']
+            q3 = hw2Form.cleaned_data['q3']
+            q4 = hw2Form.cleaned_data['q4']
+            q5 = hw2Form.cleaned_data['q5']
+            points = grade(Hw2_key, {'q1': q1, 'q2': q2, 'q3': q3, 'q4': q4, 'q5': q5})
+            item = Hw2_submit(email=email, q1=q1, q2=q2,
+                              q3=q3, q4=q4, q5=q5,
+                              g1=points['q1'], g2=points['q2'],
+                              g3=points['q3'], g4=points['q4'],
+                              g5=points['q5'], gs=points['gs'],
+                              gp=points['gp'], hasGraded=True,
+                              time=timezone.now()
+            )
+            item.save()
+            return HttpResponseRedirect(reverse('submitted'))
+
+    # default action
+    return render(request, 'homework2.html',
+                  {'hwForm': Hw2Form()})
+
+
+######################################################################
+#                      grade function                                #
+######################################################################
+
+def grade(keyTable, answer):
+    points = {}
+    full = 0.0
+    s = 0.0
+    for name, value in answer.iteritems():
+        x = keyTable.objects.get(qname=name)
+        full += x.qpoints
+        if x.qtype == 'number':
+            if abs(value - x.numberAnswer) <= x.qtol:
+                points[name] = x.qpoints
+                s += x.qpoints
+            else:
+                points[name] = 0
+                
+        elif x.qtype == 'choice':
+            if value == x.choiceAnswer:
+                points[name] = x.qpoints
+                s += x.qpoints
+            else:
+                points[name] = 0
+        else :
+            print "wrong type"
+
+    points['gs'] = s
+    points['gp'] = s / full
+
+    return points
