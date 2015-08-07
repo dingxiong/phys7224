8db03ace820acf951cbf1e1d1a9e86beb03a4d95
dingxiong <dingxiong203@gmail.com>
2015-08-07 14:30:13 -0400

    hw1-5 ready

--- hws/settings.py
+++ hws/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secretKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # for local Apache
 # ALLOWED_HOSTS = ['128.61.58.51']
