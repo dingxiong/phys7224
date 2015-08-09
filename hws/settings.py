a203df7cbeb21cf7796ca8727d001bb3eba775f5
dingxiong <dingxiong203@gmail.com>
2015-08-09 18:06:18 -0400

    ignore

--- hws/settings.py
+++ hws/settings.py
@@ -116,11 +116,11 @@ EMAIL_PORT = 587
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-# DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Static asset configuration (choose between local and server)
-# STATIC_ROOT = 'staticfiles'
-STATIC_ROOT = '/home/xding/00git/phys7224/static_root/'
+STATIC_ROOT = 'staticfiles'
+# STATIC_ROOT = '/home/xding/00git/phys7224/static_root/'
